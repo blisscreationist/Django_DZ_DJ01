7887cb8760d763198cf4ec9434465e7eb6752b44
blisscreationist <101796532+blisscreationist@users.noreply.github.com>
2024-08-20 11:16:06 +0300

    Final_code

---
+++ ZeroCoder/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+
+def new(request):
+    return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
+
+def data(request):
+    return HttpResponse("<h1>Это страница Data моего проекта на Django</h1>")
+
+def test(request):
+    return HttpResponse("<h1>Это страница Test моего проекта на Django</h1>")
